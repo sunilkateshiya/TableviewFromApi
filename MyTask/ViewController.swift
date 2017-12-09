29702081e2cb5d0e41de6031567cc052ea32ddd9
Rahul Panchal <rahul@rlogical.com>
2017-12-09 13:10:39 +0530

    Initial Commit

---
+++ MyTask/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyTask
+//
+//  Created by Rahul Panchal on 09/12/17.
+//  Copyright © 2017 Rahul Panchal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
